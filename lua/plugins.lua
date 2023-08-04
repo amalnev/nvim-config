c27891a9e38315169571120b7d11c992cbad2784
amalnev <a.malnev@gmail.com>
2023-08-04 21:58:25 +0300

    Added autocompletion based on data provided by language server

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,6 +51,14 @@ return require('packer').startup(function(use)
     }
     use 'mfussenegger/nvim-dap'
     use 'mfussenegger/nvim-jdtls'
+    use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }    
+    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' } 
+    use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }        -- buffer auto-completion
+    use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }          -- path auto-completion
+    use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' }       -- cmdline auto-completion
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+
     ---------------------------------------
     -- NOTE: PUT YOUR THIRD PLUGIN HERE --
     ---------------------------------------
